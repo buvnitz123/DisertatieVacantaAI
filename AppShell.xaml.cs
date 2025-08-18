437e9b04e47f01db164bd7866f93697875bcd5d3
Dragos Chiper <buvnitz123@gmail.com>
2025-08-19 00:10:37 +0300

    Initialising

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppDisertatieVacantaAI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace MauiAppDisertatieVacantaAI
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                Debug.WriteLine($"Clicked {count} time");
+            else
+                Debug.WriteLine($"Clicked {count} times");
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
